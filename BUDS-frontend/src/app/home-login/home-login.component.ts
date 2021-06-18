3c927bcd06393802e8fbf6ebe7a3f53746abe416
rakhikrishna <rakhipms@gmail.com>
2021-06-18 22:02:49 +0530

    home-login added without authservice

--- BUDS-frontend/src/app/home-login/home-login.component.ts
+++ BUDS-frontend/src/app/home-login/home-login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-home-login',
@@ -8,82 +8,19 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./home-login.component.css']
 })
 export class HomeLoginComponent implements OnInit {
-
   user = {
     role:'',
     email: '',
     password: ''
   };
 
-  loginState = 'none';
-  errorMsg = '';
-
-  constructor(private auth:AuthService, private router:Router) { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
-    // If already logged in, get user role from server and redirect to apropriate page
-    if(this.auth.isLoggedIn()){
-      this.auth.getRole()
-      .subscribe(
-        (data:any)=>{
-          if(data.role == 'admin'){
-            this.router.navigate(['/admin']);
-          }
-          else if(data.role == 'trainer'){
-            this.router.navigate(['/trainer']);
-          }
-          else{
-            console.log(data);
-          }
-        }
-      )
-    }
   }
 
-  resetForm(){
-    this.user = {
-      role:'',
-      email: '',
-      password: ''
-    };
-    this.loginState = 'none';
-    this.errorMsg = '';
-  }
 
-  login() {
-    this.loginState = 'connecting';
-    this.auth.login(this.user)
-    .subscribe(
-      data =>{
-        if(data.role == 'admin'){
-          localStorage.setItem('token',data.token);
-          this.resetForm();
-          this.loginState = 'success';
-          this.router.navigate(['/admin'])
-        }
-        else if(data.role == 'trainer'){
-          localStorage.setItem('token',data.token);
-          this.resetForm();
-          this.loginState = 'success';
-          this.router.navigate(['/trainer'])
-        }
-        else{
-          this.loginState = 'error'
-          this.errorMsg = 'Sorry! Something went wrong.'
-          console.log(data);
-        }
-      },
-      error =>{
-        this.loginState = 'error'
-        if(error.status === 401){
-          this.errorMsg = error.error;
-        }
-        else{
-          this.errorMsg = 'Sorry! Something went wrong.'
-          console.log(error);
-        }
-      }
-    )
-  }
-
-}
+login()
+{
+  alert("you have successfully logged in");
+}}
